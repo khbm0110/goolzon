b2639faed7f1785a70d1f6552f39e8d47b7e08a6
khbm0110 <khalidbm011@gmail.com>
2026-07-18 02:54:53 +0100

    Delete config directory

--- config/apiFootballConfig.ts
+++
@@ -1,4 +0,0 @@
-// This file is deprecated. The API key is now stored securely in environment variables
-// on the server and accessed via serverless functions in the /api directory.
-// This file can be safely deleted.
-export const API_FOOTBALL_KEY = null;
